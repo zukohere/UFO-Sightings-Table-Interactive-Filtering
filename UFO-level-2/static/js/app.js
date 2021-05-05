07073b240c5bde34ff19bbb8ad7a4400c50f0417
Matt Brown <matthewbrown84@gmail.com>
2021-05-05 16:23:22 -0400

    Level 2 complete.

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -9,50 +9,53 @@ var tbody = d3.select("tbody");
 tableData.forEach((UFOsighting) => {
     var row = tbody.append("tr");
     Object.entries(UFOsighting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+        var cell = row.append("td");
+        cell.text(value);
     });
-  });
+});
 
 // Select the button
 var button = d3.select("#filter-btn"); //arg based on button id, index line 44.
 
-// Select the form
-var form = d3.select("#form");
 
-// Create event handlers 
+// Create event handler 
 button.on("click", runEnter);
-form.on("submit",runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+    // Select the input element and get the raw HTML node
+    const idColmap = [{ id: "#datetime", col: "datetime" },
+    { id: "#city", col: "city" },
+    { id: "#state", col: "state" },
+    { id: "#country", col: "country" },
+    { id: "#shape", col: "shape" }];
 
-  console.log(inputValue);
-// filter the data to the user selection
-  var filteredData = tableData.filter(sighting => 
-    sighting.datetime === inputValue);
+    var filteredData = tableData;
 
-//print the filtered data to the console
-  console.log(filteredData); 
-
-  //overwrite table with filtered data
-  var tbody = d3.select("tbody");
-  tbody.html("") // clears the table
-
-  filteredData.forEach((UFOsighting_bydate) => {
-    var row = tbody.append("tr");
-    Object.entries(UFOsighting_bydate).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+    idColmap.forEach(entry => {
+        //if the user selection is NOT blank, filter data to user selection.
+        if (d3.select(entry.id).property("value")) {
+            filteredData = filteredData.filter(sighting =>
+                sighting[entry.col] === d3.select(entry.id).property("value"));
+        }
     });
-  })
-};
+
+    //print the filtered data to the console
+    console.log(filteredData);
+
+    //overwrite table with filtered data
+    var tbody = d3.select("tbody");
+    tbody.html(""); // clears the table
+
+    filteredData.forEach((UFOsighting_bydate) => {
+        var row = tbody.append("tr");
+        Object.entries(UFOsighting_bydate).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    })
+}
